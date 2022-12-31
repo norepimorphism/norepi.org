f9b5a8b4357b8e222b5bf7ac3df66468cb092889
norepimorphism <norepi@protonmail.com>
2022-12-31 17:41:38 -0500

    http: check if request is well-formed after accessing resource

--- src/bin/http/main.rs
+++ src/bin/http/main.rs
@@ -182,309 +182,13 @@ fn respond(req: Request<Body>) -> Result<Response<Body>, http::Error> {
         req.uri(),
     );
 
-    let mut res = match check_request_is_well_formed(&req) {
-        Ok(_) => respond_to_well_formed_request(&req)?,
-        Err(res) => {
-            tracing::error!("request was malformed");
-
-            // The request was malformed, so a response containing error information was returned.
-            res?
-        }
-    };
-
-    // Append default response headers.
-
-    let headers = res.headers_mut();
-    // RFC 9110, Section 10.2.4:
-    //   The "Server" header field contains information about the software used by the origin server
-    //   to handle the request.... An origin server MAY generate a Server header field in its
-    //   responses.
-    //
-    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.10.2.4>.
-    headers.insert(header::SERVER, HeaderValue::from_static(SERVER));
-
-    Ok(res)
-}
-
-fn check_request_is_well_formed(
-    req: &Request<Body>,
-) -> Result<(), Result<Response<Body>, http::Error>> {
-    let headers = req.headers();
-
-    // RFC 9110, Section 15.5.7:
-    //   The 406 (Not Acceptable) status code indicates that the target resource does not have a
-    //   current representation that would be acceptable to the user agent, according to the
-    //   proactive negotiation header fields received... and the server is unwilling to supply a
-    //   default representation.
-    //
-    //   The server SHOULD generate contant containing a list of representation characteristics and
-    //   corresponding resource identifiers from which the user or user agent can choose the one
-    //   most appropriate.
-    //
-    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.15.5.7>.
-    let not_acceptable = || {
-        Err(resource::include!("406"."html").status(StatusCode::NOT_ACCEPTABLE).build().response())
-    };
-
-    // RFC 9110, Section 12.5.2:
-    //   Note: Accept-Charset is deprecated because UTF-8 has become nearly ubiquitous.... Most
-    //   general-purpose user agents do not send Accept-Charset unless specifically configured to do
-    //   so.
-    //
-    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.2>.
-    if let Some(value) = headers.get(header::ACCEPT_CHARSET) {
-        tracing::warn!("Accept-Charset was received but is deprecated");
-
-        // Despite the deprecation, in the interest of compatibility, we will accept
-        // `Accept-Charset` headers that request UTF-8.
-        if !accept_charset_requests_utf8(value) {
-            tracing::error!("Accept-Charset header does not request UTF-8");
-            return not_acceptable();
-        }
-    }
-
-    // RFC 9110, Section 12.5.3:
-    //   When sent by a user agent in a request, Accept-Encoding indicates the content codings
-    //   acceptable in a response.
-    //   ...
-    //   When sent by a server in response, Accept-Encoding provides information about which content
-    //   codings are preferred in the context of a subsequent request to the same resource.
-    //   ...
-    //   An "identity" token is used as a synonym for "no encoding" in order to communicate when no
-    //   encoding is preferred.
-    //   ...
-    //   If no Accept-Encoding header field is in the request, any content coding is considered
-    //   acceptable by the user agent.
-    //
-    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.3>.
-    if let Some(value) = headers.get(header::ACCEPT_ENCODING) {
-        // Resource content will be returned as-is; hence, we will only accept `Accept-Encoding`
-        // headers that request `identity`.
-        if !accept_encoding_requests_identity(value) {
-            tracing::error!("Accept-Encoding header does not request identity");
-            // RFC 9110, Section 12.5.3:
-            //   Servers that fail a request due to an unsupported content coding ought to respond
-            //   with a 415 (Unsupported Media Type) status and include an Accept-Encoding header
-            //   field in that response, allowing clients to distinguish between issues related to
-            //   content codings and media types.
-            return Err({
-                resource::include!("415"."html")
-                    .status(StatusCode::UNSUPPORTED_MEDIA_TYPE)
-                    .build()
-                    .response()
-                    .map(|mut res| {
-                        res.headers_mut().insert(
-                            header::ACCEPT_ENCODING,
-                            HeaderValue::from_static("identity"),
-                        );
-
-                        res
-                    })
-            });
-        }
-    }
-
-    // RFC 9110, Section 12.5.4:
-    //   The "Accept-Language" header field can be used by user agents to indicate the set of
-    //   natural languages that are preferred in the response.
-    //
-    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.4>.
-    if let Some(value) = headers.get(header::ACCEPT_LANGUAGE) {
-        if !accept_language_requests_en(value) {
-            tracing::error!("Accept-Language header does not request en");
-            return not_acceptable();
-        }
-    }
-
-    Ok(())
-}
-
-fn accept_charset_requests_utf8(value: &HeaderValue) -> bool {
-    let Ok(prefs) = iter_accept_prefs(value) else {
-        // The header is malformed.
-        return false;
-    };
-
-    for pref in prefs {
-        // RFC 9110, Section 12.5.2:
-        //   The special value "*", if present in the Accept-Charset header field, matches every
-        //   charset that is not mentioned elsewhere in the field.
-        if pref.is_acceptable_with_name(b"*") {
-            // If a `*` is present---even if it has a low priority---UTF-8 will be requested. Either
-            // both UTF-8 and `*` are listed, in which case UTF-8 is obviously requested, or only
-            // `*` is listed, in which case UTF-8 is indirectly requested through use of this
-            // wildcard.
-            return true;
-        }
-
-        // We will also check if the field contains the case-insensitive literal "UTF-8", in which
-        // case UTF-8 is explicitly requested.
-        if pref.is_acceptable_with_name(b"utf-8") {
-            return true;
-        }
-    }
-
-    false
-}
-
-fn accept_encoding_requests_identity(value: &HeaderValue) -> bool {
-    // RFC 9110, Section 12.5.3:
-    //   An Accept-Encoding header field with a field value that is empty implies that the user
-    //   agent does not want any content coding in response.
-    if value.is_empty() {
-        // Ostensibly, "does not want any content coding" implies the `identity` value.
-        return true;
-    }
-
-    let Ok(prefs) = iter_accept_prefs(value) else {
-        // The header is malformed.
-        return false;
-    };
-
-    // RFC 9110, Section 12.5.3:
-    //   If the representation has no content coding, then it is acceptable by default unless
-    //   specifically excluded by the Accept-Encoding header field stating either "identity;q=0" or
-    //   "*;q=0" without a more specific entry for "identity".
-
-    for pref in prefs {
-        // RFC 9110, Section 12.5.3:
-        //   The asterisk "*" symbol in an Accept-Encoding field matches any available content
-        //   coding not explicitly listed in the field.
-        if pref.is_inacceptable_with_name(b"*") {
-            return false;
-        }
-
-        if pref.is_inacceptable_with_name(b"identity") {
-            return false;
-        }
-    }
-
-    true
-}
-
-fn accept_language_requests_en(value: &HeaderValue) -> bool {
-    let Ok(prefs) = iter_accept_prefs(value) else {
-        // The header is malformed.
-        return false;
-    };
-
-    for pref in prefs {
-        // RFC 4647, Section 3.3.1:
-        //   The special range "*" in a language priority list matches any tag.
-        //
-        // See <https://www.rfc-editor.org/rfc/rfc4647.html#section-3.3.1>.
-        if pref.is_acceptable_with_name(b"*") {
-            return true;
-        }
-
-        // We will also check if the field contains the case-insensitive literal "en", in which
-        // English is explicitly requested.
-        if pref.is_acceptable_with_name(b"en") {
-            return true;
-        }
-    }
-
-    false
-}
-
-fn iter_accept_prefs<'a>(
-    value: &'a HeaderValue,
-) -> Result<impl 'a + Iterator<Item = AcceptPreference<'a>>, ()> {
-    value
-        .as_bytes()
-        // Preferences are separated by commas `,`. There are no trailing commas.
-        .split(|c| *c == b',')
-        // Names and qparams are separated by semicolons `;`. There are no trailing semicolons.
-        .map(|pref| pref.split(|c| *c == b';'))
-        .map(|mut parts| {
-            // The first part of a preference is the name.
-            let name = parts
-                .next()
-                // The name may be prefixed by whitespace.
-                .map(|it| it.trim_ascii_start())
-                // It is an error if the name is not present.
-                .ok_or(())?;
-            // The second part of a preference is the qparam. It is optional.
-            let qparam = parts.next();
-            let qvalue = match qparam {
-                Some(it) => Some({
-                    it
-                        // The qparam may be prefixed by whitespace.
-                        .trim_ascii_start()
-                        // The qvalue is always prefixed by the string "q=". This string must appear
-                        // as-is without alternative capitalization and without whitespace.
-                        .strip_prefix(b"q=")
-                        // It is an error if the "q=" prefix is not present.
-                        .ok_or(())?
-                }),
-                None => None,
-            };
-            // It is an error if any additional parts are present.
-            if parts.next().is_some() {
-                return Err(());
-            }
-
-            Ok(AcceptPreference { name, qvalue })
-        })
-        .collect::<Vec<Result<AcceptPreference, ()>>>()
-        .into_iter()
-        .collect::<Result<Vec<AcceptPreference>, ()>>()
-        .map(|inner| inner.into_iter())
-}
-
-struct AcceptPreference<'a> {
-    name: &'a [u8],
-    qvalue: Option<&'a [u8]>,
-}
-
-impl fmt::Display for AcceptPreference<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", std::str::from_utf8(self.name).unwrap_or("<err>"))?;
-        if let Some(qvalue) = self.qvalue {
-            write!(f, ";q={}", std::str::from_utf8(qvalue).unwrap_or("<err>"))?;
-        }
-
-        Ok(())
-    }
-}
-
-impl<'a> AcceptPreference<'a> {
-    fn is_acceptable_with_name(&'a self, name: &[u8]) -> bool {
-        // We are careful to compare insensitively.
-        self.name.eq_ignore_ascii_case(name) && self.is_acceptable()
-    }
-
-    fn is_inacceptable_with_name(&'a self, name: &[u8]) -> bool {
-        // Note that this is *not* the same as `!is_acceptable_with_name`.
-        self.name.eq_ignore_ascii_case(name) && self.is_inacceptable()
-    }
-
-    fn is_acceptable(&'a self) -> bool {
-        // RFC 9110, Section 12.4.2:
-        //   The weight is normalized to a real number in the range 0 through 1, where 0.001 is the
-        //   least preferred and 1 is the most preferred; a value of 0 means "not acceptable". If no
-        //   "q" parameter is present, the default weight is 1.
-        self.qvalue.map(|val| {
-            !matches!(val, b"0" | b"0." | b"0.0" | b"0.00" | b"0.000")
-        })
-        .unwrap_or(true)
-    }
-
-    fn is_inacceptable(&'a self) -> bool {
-        !self.is_acceptable()
-    }
-}
-
-fn respond_to_well_formed_request(
-    req: &Request<Body>,
-) -> Result<Response<Body>, http::Error> {
-    match req.method() {
+    let response = match req.method() {
         // RFC 9110, Section 9.3.1:
         //   The GET method request transfer of a current selected representation for the target
         //   resource.
         //
         // See <https://httpwg.org/specs/rfc9110.html#rfc.section.9.3.1>.
-        &Method::GET => get(req).response(),
+        &Method::GET => get(&req).response(),
         // RFC 9110, Section 9.3.2:
         //   The HEAD method is identical to GET except that the server MUST NOT send content in the
         //   response.
@@ -503,7 +207,7 @@ fn respond_to_well_formed_request(
             // TODO: Our approach is to generate a GET response and discard the body and irrelevant
             // fields, though RFC 9110 indicates that this is not preferred. Is there a signficiant
             // performance cost to doing this?
-            get(req).map(|mut res| {
+            get(&req).response().map(|mut res| {
                 // Discard the body.
                 *res.body_mut() = Body::empty();
 
@@ -539,11 +243,24 @@ fn respond_to_well_formed_request(
                 .build()
                 .response()
         }
-    }
+    }?;
+
+    // Append default response headers.
+
+    let headers = response.headers_mut();
+    // RFC 9110, Section 10.2.4:
+    //   The "Server" header field contains information about the software used by the origin server
+    //   to handle the request.... An origin server MAY generate a Server header field in its
+    //   responses.
+    //
+    // See <https://httpwg.org/specs/rfc9110.html#rfc.section.10.2.4>.
+    headers.insert(header::SERVER, HeaderValue::from_static(SERVER));
+
+    Ok(response)
 }
 
-fn get(req: &Request<Body>) -> Resource {
-    match req.uri().path() {
+fn get(req: &Request<Body>) -> Result<Response<Body>, http::Error> {
+    let resource = match req.uri().path() {
         "/" => resource::include!("index"."html"),
         "/base.css" => resource::include!("base"."css"),
         "/error.css" => resource::include!("error"."css"),
@@ -553,5 +270,15 @@ fn get(req: &Request<Body>) -> Resource {
         "/robots.txt" => resource::include!("robots"."txt"),
         _ => resource::include!("404"."html").status(StatusCode::NOT_FOUND),
     }
-    .build()
+    .build();
+
+    match resource.check_request_is_well_formed(&req) {
+        Ok(_) => resource.response(),
+        Err(res) => {
+            tracing::error!("request was malformed");
+
+            // The request was malformed, so a response containing error information was returned.
+            res
+        }
+    }
 }
--- src/bin/http/resource.rs
+++ src/bin/http/resource.rs
@@ -4,10 +4,10 @@ use hyper::{header, http, Body, Response, StatusCode};
 
 pub mod content_type;
 
-pub use include;
+pub use _include as include;
 pub use include_content;
 
-macro_rules! include {
+macro_rules! _include {
     ($file_base:literal . $file_ext:literal $(,)?) => {
         Builder::new(content_type::from_file_ext($file_ext))
             .content($crate::resource::include_content!(concat!($file_base, ".", $file_ext)))
@@ -41,6 +41,7 @@ impl Builder {
         Self {
             content_type,
             language: None,
+            charset: None,
             content: None,
             status_code: None,
         }
@@ -50,6 +51,7 @@ impl Builder {
 pub struct Builder {
     content_type: &'static str,
     language: Option<&'static str>,
+    charset: Option<&'static str>,
     content: Option<&'static str>,
     status_code: Option<StatusCode>,
 }
@@ -57,6 +59,12 @@ pub struct Builder {
 impl Builder {
     pub fn language(&mut self, code: &'static str) -> &mut Self {
         self.language = Some(code);
+
+        self
+    }
+
+    pub fn charset(&mut self, charset: &'static str) -> &mut Self {
+        self.charset = Some(charset);
 
         self
     }
@@ -77,6 +85,7 @@ impl Builder {
         Resource {
             content_type: self.content_type,
             language: self.language.unwrap_or("en"),
+            charset: self.charset.unwrap_or("utf-8"),
             content: self.content.unwrap_or_default(),
             status_code: self.status_code.unwrap_or(StatusCode::OK),
         }
@@ -86,6 +95,7 @@ impl Builder {
 pub struct Resource {
     pub content_type: &'static str,
     pub language: &'static str,
+    pub charset: &'static str,
     pub content: &'static str,
     pub status_code: StatusCode,
 }
@@ -105,5 +115,267 @@ impl Resource {
             // See <https://httpwg.org/specs/rfc9110.html#rfc.section.8.5>.
             .header(header::CONTENT_LANGUAGE, self.language)
             .body(self.content.into())
+    }
+
+    pub fn check_request_is_well_formed(
+        &self,
+        req: &Request<Body>,
+    ) -> Result<(), Result<Response<Body>, http::Error>> {
+        let headers = req.headers();
+
+        // RFC 9110, Section 15.5.7:
+        //   The 406 (Not Acceptable) status code indicates that the target resource does not have a
+        //   current representation that would be acceptable to the user agent, according to the
+        //   proactive negotiation header fields received... and the server is unwilling to supply a
+        //   default representation.
+        //
+        //   The server SHOULD generate contant containing a list of representation characteristics
+        //   and corresponding resource identifiers from which the user or user agent can choose the
+        //   one most appropriate.
+        //
+        // See <https://httpwg.org/specs/rfc9110.html#rfc.section.15.5.7>.
+        let not_acceptable = || {
+            Err(_include!("406"."html").status(StatusCode::NOT_ACCEPTABLE).build().response())
+        };
+
+        // RFC 9110, Section 12.5.2:
+        //   Note: Accept-Charset is deprecated because UTF-8 has become nearly ubiquitous.... Most
+        //   general-purpose user agents do not send Accept-Charset unless specifically configured
+        //   to do so.
+        //
+        // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.2>.
+        if let Some(value) = headers.get(header::ACCEPT_CHARSET) {
+            tracing::warn!("Accept-Charset was received but is deprecated");
+
+            if !self.matches_accept_charset(value) {
+                tracing::error!("Accept-Charset header does not request {}", self.charset);
+                return not_acceptable();
+            }
+        }
+
+        // RFC 9110, Section 12.5.3:
+        //   When sent by a user agent in a request, Accept-Encoding indicates the content codings
+        //   acceptable in a response.
+        //   ...
+        //   When sent by a server in response, Accept-Encoding provides information about which
+        //   content codings are preferred in the context of a subsequent request to the same
+        //   resource.
+        //   ...
+        //   An "identity" token is used as a synonym for "no encoding" in order to communicate when
+        //   no encoding is preferred.
+        //   ...
+        //   If no Accept-Encoding header field is in the request, any content coding is considered
+        //   acceptable by the user agent.
+        //
+        // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.3>.
+        if let Some(value) = headers.get(header::ACCEPT_ENCODING) {
+            // Resource content will be returned as-is; hence, we will only accept `Accept-Encoding`
+            // headers that request `identity`.
+            if !self.matches_accept_encoding(value) {
+                tracing::error!("Accept-Encoding header does not request identity");
+                // RFC 9110, Section 12.5.3:
+                //   Servers that fail a request due to an unsupported content coding ought to
+                //   respond with a 415 (Unsupported Media Type) status and include an
+                //   Accept-Encoding header field in that response, allowing clients to distinguish
+                //   between issues related to content codings and media types.
+                return Err({
+                    _include!("415"."html")
+                        .status(StatusCode::UNSUPPORTED_MEDIA_TYPE)
+                        .build()
+                        .response()
+                        .map(|mut res| {
+                            res.headers_mut().insert(
+                                header::ACCEPT_ENCODING,
+                                HeaderValue::from_static("identity"),
+                            );
+
+                            res
+                        })
+                });
+            }
+        }
+
+        // RFC 9110, Section 12.5.4:
+        //   The "Accept-Language" header field can be used by user agents to indicate the set of
+        //   natural languages that are preferred in the response.
+        //
+        // See <https://httpwg.org/specs/rfc9110.html#rfc.section.12.5.4>.
+        if let Some(value) = headers.get(header::ACCEPT_LANGUAGE) {
+            if !self.matches_accept_language(value) {
+                tracing::error!("Accept-Language header does not request {}", self.language);
+                return not_acceptable();
+            }
+        }
+
+        Ok(())
+    }
+
+    fn matches_accept_charset(&self, value: &HeaderValue) -> bool {
+        let Ok(prefs) = iter_accept_prefs(value) else {
+            // The header is malformed.
+            return false;
+        };
+
+        for pref in prefs {
+            // RFC 9110, Section 12.5.2:
+            //   The special value "*", if present in the Accept-Charset header field, matches every
+            //   charset that is not mentioned elsewhere in the field.
+            if pref.is_acceptable_with_name(b"*") {
+                return true;
+            }
+
+            if pref.is_acceptable_with_name(self.charset.as_bytes()) {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    // TODO: Only supports identity.
+    fn matches_accept_encoding(value: &HeaderValue) -> bool {
+        // RFC 9110, Section 12.5.3:
+        //   An Accept-Encoding header field with a field value that is empty implies that the user
+        //   agent does not want any content coding in response.
+        if value.is_empty() {
+            // Ostensibly, "does not want any content coding" implies the `identity` value.
+            return true;
+        }
+
+        let Ok(prefs) = iter_accept_prefs(value) else {
+            // The header is malformed.
+            return false;
+        };
+
+        // RFC 9110, Section 12.5.3:
+        //   If the representation has no content coding, then it is acceptable by default unless
+        //   specifically excluded by the Accept-Encoding header field stating either "identity;q=0"
+        //   or "*;q=0" without a more specific entry for "identity".
+
+        for pref in prefs {
+            // RFC 9110, Section 12.5.3:
+            //   The asterisk "*" symbol in an Accept-Encoding field matches any available content
+            //   coding not explicitly listed in the field.
+            if pref.is_inacceptable_with_name(b"*") {
+                return false;
+            }
+
+            if pref.is_inacceptable_with_name(b"identity") {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn matches_accept_language(&self, value: &HeaderValue) -> bool {
+        let Ok(prefs) = iter_accept_prefs(value) else {
+            // The header is malformed.
+            return false;
+        };
+
+        for pref in prefs {
+            // RFC 4647, Section 3.3.1:
+            //   The special range "*" in a language priority list matches any tag.
+            //
+            // See <https://www.rfc-editor.org/rfc/rfc4647.html#section-3.3.1>.
+            if pref.is_acceptable_with_name(b"*") {
+                return true;
+            }
+
+            if pref.is_acceptable_with_name(self.language.as_bytes()) {
+                return true;
+            }
+        }
+
+        false
+    }
+}
+
+fn iter_accept_prefs<'a>(
+    value: &'a HeaderValue,
+) -> Result<impl 'a + Iterator<Item = AcceptPreference<'a>>, ()> {
+    value
+        .as_bytes()
+        // Preferences are separated by commas `,`. There are no trailing commas.
+        .split(|c| *c == b',')
+        // Names and qparams are separated by semicolons `;`. There are no trailing semicolons.
+        .map(|pref| pref.split(|c| *c == b';'))
+        .map(|mut parts| {
+            // The first part of a preference is the name.
+            let name = parts
+                .next()
+                // The name may be prefixed by whitespace.
+                .map(|it| it.trim_ascii_start())
+                // It is an error if the name is not present.
+                .ok_or(())?;
+            // The second part of a preference is the qparam. It is optional.
+            let qparam = parts.next();
+            let qvalue = match qparam {
+                Some(it) => Some({
+                    it
+                        // The qparam may be prefixed by whitespace.
+                        .trim_ascii_start()
+                        // The qvalue is always prefixed by the string "q=". This string must appear
+                        // as-is without alternative capitalization and without whitespace.
+                        .strip_prefix(b"q=")
+                        // It is an error if the "q=" prefix is not present.
+                        .ok_or(())?
+                }),
+                None => None,
+            };
+            // It is an error if any additional parts are present.
+            if parts.next().is_some() {
+                return Err(());
+            }
+
+            Ok(AcceptPreference { name, qvalue })
+        })
+        .collect::<Vec<Result<AcceptPreference, ()>>>()
+        .into_iter()
+        .collect::<Result<Vec<AcceptPreference>, ()>>()
+        .map(|inner| inner.into_iter())
+}
+
+struct AcceptPreference<'a> {
+    name: &'a [u8],
+    qvalue: Option<&'a [u8]>,
+}
+
+impl fmt::Display for AcceptPreference<'_> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", std::str::from_utf8(self.name).unwrap_or("<err>"))?;
+        if let Some(qvalue) = self.qvalue {
+            write!(f, ";q={}", std::str::from_utf8(qvalue).unwrap_or("<err>"))?;
+        }
+
+        Ok(())
+    }
+}
+
+impl<'a> AcceptPreference<'a> {
+    fn is_acceptable_with_name(&'a self, name: &[u8]) -> bool {
+        // We are careful to compare insensitively.
+        self.name.eq_ignore_ascii_case(name) && self.is_acceptable()
+    }
+
+    fn is_inacceptable_with_name(&'a self, name: &[u8]) -> bool {
+        // Note that this is *not* the same as `!is_acceptable_with_name`.
+        self.name.eq_ignore_ascii_case(name) && self.is_inacceptable()
+    }
+
+    fn is_acceptable(&'a self) -> bool {
+        // RFC 9110, Section 12.4.2:
+        //   The weight is normalized to a real number in the range 0 through 1, where 0.001 is the
+        //   least preferred and 1 is the most preferred; a value of 0 means "not acceptable". If no
+        //   "q" parameter is present, the default weight is 1.
+        self.qvalue.map(|val| {
+            !matches!(val, b"0" | b"0." | b"0.0" | b"0.00" | b"0.000")
+        })
+        .unwrap_or(true)
+    }
+
+    fn is_inacceptable(&'a self) -> bool {
+        !self.is_acceptable()
     }
 }
